1077496ffcad2f7e4fab3c6da0a2f99534d12b02
linh5942 <linh594@newschool.edu>
2019-09-07 23:02:49 +0800

    test3

--- NuEIP-test/src/test.java
+++ NuEIP-test/src/test.java
@@ -1,33 +1,37 @@
 import java.util.Arrays;
-
-
+import java.util.ArrayList;
 
 public class test {
 
 	public static void main(String[] args) {
 		
-		int array[] = {77,5,5,22,13,55,97,4,796,1,0,9};
+		int array[] = {0,1,2,3,4,5,6,7,8,9};
+		
+		ArrayList evenArray = new ArrayList();
+		ArrayList oddArray = new ArrayList();
+		
+		int oddSum = 0;
+		int evenSum = 0;
+		int answer=0;
+		
+		for(int i=0;i<array.length;i+=2) {
+			evenSum += i;
+			evenArray.add(array[i]);
+			
+		}
+		
+		for(int i=1;i<array.length;i+=2) {
+			oddSum += i;
+			oddArray.add(array[i]);
+		}
+		
+		answer = oddSum - evenSum;
+		
+		System.out.println(answer);	
+		System.out.println(evenArray);
+		System.out.println(oddArray);
 		
 			
-		
-		boolean sorted = false;
-		int temp;
-		while(!sorted) {
-			sorted = true;
-			for(int i=0; i<array.length-1; i++) {
-				if(array[i]>array[i+1]) {
-					temp = array[i];
-					array[i] = array[i+1];
-					array[i+1] = temp;
-					sorted = false;
-				}
-			}
-		}
-				
-		System.out.println(Arrays.toString(array));
-		
-		
-		
 		}
 
 }
